ba137a4cc43d1cf9be2e10a0129b82b75cae3425
Golovin.Denis <golovin.denis@innovacia.ru>
2023-05-02 01:48:14 +0700

    init

---
+++ src/main/java/golovin/store/gusli/GusliApplication.java
@@ -0,0 +1,13 @@
+package golovin.store.gusli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/golovin/store/gusli/GusliApplicationTests.java
@@ -0,0 +1,13 @@
+package golovin.store.gusli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GusliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
