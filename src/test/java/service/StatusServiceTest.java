529b111d1ad96e05625bd401089b637a3f176a17
Deonniss <dengolovin2017@gmail.com>
2023-06-05 15:35:03 +0700

    tests for StatusService

---
+++ src/test/java/service/StatusServiceTest.java
@@ -0,0 +1,34 @@
+package service;
+
+import golovin.store.gusli.entity.Status;
+import golovin.store.gusli.entity.type.StatusType;
+import golovin.store.gusli.repository.StatusRepository;
+import golovin.store.gusli.service.StatusService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class StatusServiceTest {
+
+    @Mock
+    private StatusRepository statusRepository;
+
+    @Test
+    public void testGetByType() {
+        StatusService statusService = new StatusService(statusRepository);
+        Status expectedStatus = Status.builder()
+                .id(1L)
+                .name("create")
+                .type(StatusType.CREATED)
+                .description("create")
+                .build();
+        when(statusRepository.findByType(StatusType.CREATED)).thenReturn(expectedStatus);
+        Status actualStatus = statusService.getByType(StatusType.CREATED);
+        assertEquals(expectedStatus, actualStatus);
+    }
+}
