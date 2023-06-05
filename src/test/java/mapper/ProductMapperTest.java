aea86c3f665d54249a4e870b3d4612f3642c1562
Deonniss <dengolovin2017@gmail.com>
2023-06-05 14:07:19 +0700

    tests for ProductMapper

--- src/test/java/mapper/ProductMapperTest.java
+++ src/test/java/mapper/ProductMapperTest.java
@@ -1,5 +1,6 @@
 package mapper;
 
+import golovin.store.gusli.dto.CategoryDto;
 import golovin.store.gusli.dto.ProductDto;
 import golovin.store.gusli.entity.Category;
 import golovin.store.gusli.entity.Product;
@@ -9,6 +10,8 @@ import org.mapstruct.factory.Mappers;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -16,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class ProductMapperTest {
 
     private final ProductMapper productMapper = Mappers.getMapper(ProductMapper.class);
+
+    private static final Timestamp timestamp = Timestamp.from(Instant.now());
 
     @Test
     public void testToEntity() {
@@ -31,8 +36,8 @@ public class ProductMapperTest {
                 .builder()
                 .id(1L)
                 .name("name")
-                .createdAt(Timestamp.from(Instant.now()))
-                .updatedAt(Timestamp.from(Instant.now()))
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
                 .build();
 
         Product product = productMapper.toEntity(productDto, category);
@@ -54,6 +59,7 @@ public class ProductMapperTest {
     @Test
     public void testUpdateEntity() {
         // Создание тестовых данных
+
         ProductDto productDto = ProductDto
                 .builder()
                 .name("new name")
@@ -65,11 +71,10 @@ public class ProductMapperTest {
                 .builder()
                 .id(1L)
                 .name("name")
-                .createdAt(Timestamp.from(Instant.now()))
-                .updatedAt(Timestamp.from(Instant.now()))
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
                 .build();
 
-        Timestamp timestamp = Timestamp.from(Instant.now());
 
         Product product = Product
                 .builder()
@@ -100,15 +105,128 @@ public class ProductMapperTest {
     @Test
     public void testToDto() {
 
+        Category category = Category
+                .builder()
+                .id(1L)
+                .name("name")
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        Product product = Product
+                .builder()
+                .id(1L)
+                .name("name")
+                .price(100.5d)
+                .description("description ...")
+                .avgRating(1.5)
+                .totalReview(2)
+                .category(category)
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        ProductDto productDto = productMapper.toDto(product);
+
+        assertEquals(productDto.getId(), product.getId());
+        assertEquals(productDto.getName(), product.getName());
+        assertEquals(productDto.getPrice(), product.getPrice());
+        assertEquals(productDto.getDescription(), product.getDescription());
+        assertEquals(productDto.getAvgRating(), product.getAvgRating());
+        assertEquals(productDto.getTotalReview(), product.getTotalReview());
+        assertEquals(productDto.getCategory().getId(), category.getId());
+        assertEquals(productDto.getCategory().getName(), category.getName());
+        assertEquals(productDto.getCategory().getUpdatedAt(), category.getUpdatedAt());
+        assertEquals(productDto.getCategory().getCreatedAt(), category.getCreatedAt());
+        assertEquals(productDto.getUpdatedAt(), timestamp);
+        assertEquals(productDto.getCreatedAt(), timestamp);
     }
 
     @Test
     public void testToDtos() {
 
+        Category category = Category
+                .builder()
+                .id(1L)
+                .name("name")
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        Product product = Product
+                .builder()
+                .id(1L)
+                .name("name")
+                .price(100.5d)
+                .description("description ...")
+                .avgRating(1.5)
+                .totalReview(2)
+                .category(category)
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+
+        List<ProductDto> productDtos = productMapper.toDtos(products);
+        ProductDto productDto = productDtos.get(0);
+
+        assertEquals(productDto.getId(), product.getId());
+        assertEquals(productDto.getName(), product.getName());
+        assertEquals(productDto.getPrice(), product.getPrice());
+        assertEquals(productDto.getDescription(), product.getDescription());
+        assertEquals(productDto.getAvgRating(), product.getAvgRating());
+        assertEquals(productDto.getTotalReview(), product.getTotalReview());
+        assertEquals(productDto.getCategory().getId(), category.getId());
+        assertEquals(productDto.getCategory().getName(), category.getName());
+        assertEquals(productDto.getCategory().getUpdatedAt(), category.getUpdatedAt());
+        assertEquals(productDto.getCategory().getCreatedAt(), category.getCreatedAt());
+        assertEquals(productDto.getUpdatedAt(), timestamp);
+        assertEquals(productDto.getCreatedAt(), timestamp);
+
     }
 
     @Test
     public void testToEntities() {
+
+        CategoryDto categoryDto = CategoryDto
+                .builder()
+                .name("name")
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        ProductDto productDto = ProductDto
+                .builder()
+                .name("name")
+                .price(100.5d)
+                .description("description ...")
+                .avgRating(1.5)
+                .totalReview(2)
+                .category(categoryDto)
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        List<ProductDto> productDtos = new ArrayList<>();
+        productDtos.add(productDto);
+
+        List<Product> products = productMapper.toEntities(productDtos);
+        Product product = products.get(0);
+
+        assertEquals(productDto.getId(), product.getId());
+        assertEquals(productDto.getName(), product.getName());
+        assertEquals(productDto.getPrice(), product.getPrice());
+        assertEquals(productDto.getDescription(), product.getDescription());
+        assertEquals(productDto.getAvgRating(), product.getAvgRating());
+        assertEquals(productDto.getTotalReview(), product.getTotalReview());
+        assertEquals(productDto.getCategory().getId(), categoryDto.getId());
+        assertEquals(productDto.getCategory().getName(), categoryDto.getName());
+        assertEquals(productDto.getCategory().getUpdatedAt(), categoryDto.getUpdatedAt());
+        assertEquals(productDto.getCategory().getCreatedAt(), categoryDto.getCreatedAt());
+        assertEquals(productDto.getUpdatedAt(), timestamp);
+        assertEquals(productDto.getCreatedAt(), timestamp);
 
     }
 
