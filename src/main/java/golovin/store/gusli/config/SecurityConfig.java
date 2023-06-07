77138fa9b21ce567a13ed1aa782cdc72a0c78941
Deonniss <dengolovin2017@gmail.com>
2023-06-07 15:42:10 +0700

    fix locked filter

--- src/main/java/golovin/store/gusli/config/SecurityConfig.java
+++ src/main/java/golovin/store/gusli/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
--- src/main/java/golovin/store/gusli/security/UserLockedFilter.java
+++ src/main/java/golovin/store/gusli/security/UserLockedFilter.java
@@ -20,7 +20,7 @@ public class UserLockedFilter extends OncePerRequestFilter {
     @SneakyThrows
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
         Long userId = (Long) request.getAttribute("userId");
-        if (userService.isLocked(userId)) {
+        if (userId != null && userService.isLocked(userId)) {
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "User has been locked");
         }
         filterChain.doFilter(request, response);
